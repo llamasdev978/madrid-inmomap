8af09f2fd9613005067f1b6c7a0d9a99787c3278
llamasdev978 <llamasdev978@gmail.com>
2025-03-31 12:40:23 +0200

    asd

--- connection.py
+++ connection.py
@@ -2,6 +2,7 @@
 import mysql.connector
 
 class DbManager:
+    # Constructor de clase para realizar la conexion
     def __init__(self, host="127.0.0.1", user="root", pwd="", db="inmomapbdd"):
         try:
             self.connection = mysql.connector.connect(
@@ -11,13 +12,25 @@ class DbManager:
                 database=db,
             )
             print("Conexión con la base de datos realizada con éxito")
-            self.cursor = self.connection.cursor()
+            self.cursor = self.connection.cursor() # Definimos el cursor para operar sobre la base
         except mysql.connector.Error as e:
             print(f"Error al conectar con la base de datos.\n{e}")
             self.connection = None
 
-        
     
+    def get_usuarios(self):
+        self.cursor.execute("SELECT * FROM usuarios")
+        return self.cursor.fetchall()
+    
+    def get_roles(self):
+        self.cursor.execute("SELECT * FROM rol")
+        return self.cursor.fetchall()
+    
+    def get_zonas(self):
+        self.cursor.execute("SELECT * FROM zonas")
+        return self.cursor.fetchall()
+
+    # Funcion para desconectarse de la base de datos
     def desconectar(self):
         self.connection.disconnect()
         print("Fin de la conexion con la base de datos")
