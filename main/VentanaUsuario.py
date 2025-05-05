ee26dab9ee331f91a51eb9edc89ac1bdceab52db
llamasdev978 <llamasdev978@gmail.com>
2025-05-05 13:31:01 +0200

    a

--- main/VentanaUsuario.py
+++ main/VentanaUsuario.py
@@ -10,5 +10,5 @@ class VentanaUsuario(tk.Frame):
         self.pack(fill="both", expand=True)
 
         fuente_titulo = tkfont.Font(family="Palatino Linotype", size=36, weight="bold")
-        titulo = tk.Label(self, text="Usuario", font=fuente_titulo, fg=AZUL_MARINO, bg=BEIGE)
+        titulo = tk.Label(self, text="pene", font=fuente_titulo, fg=AZUL_MARINO, bg=BEIGE)
         titulo.pack(pady=(80, 50))
